88559043115c2e5292264b70191005a49a9a773d
mostermouse <whdls7946@gmail.com>
2024-04-08 10:39:30 +0900

    Initial commit

---
+++ src/main/java/org/table/tableprojcet/TableprojcetApplication.java
@@ -0,0 +1,13 @@
+package org.table.tableprojcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableprojcetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableprojcetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/table/tableprojcet/TableprojcetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.table.tableprojcet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableprojcetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
