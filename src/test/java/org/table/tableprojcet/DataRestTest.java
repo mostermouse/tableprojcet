842719fd2ab79498013bd3abc6d3f174d700bb20
mostermouse <whdls7946@gmail.com>
2024-04-11 10:27:44 +0900

    통합테스트 불필요 제외처리

--- src/test/java/org/table/tableprojcet/DataRestTest.java
+++ src/test/java/org/table/tableprojcet/DataRestTest.java
@@ -1,5 +1,6 @@
 package org.table.tableprojcet;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@Disabled("Spring Data Rest 통합테스트는 불필요해서 제외시킴")
 @DisplayName("Data REST API테스트")
 //테스트 동작 롤백
 @Transactional
